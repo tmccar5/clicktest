f7cfa9f959526f0fe5fc6712ae99c7339858116d
User <User@Apples-MacBook-Pro-1113.local>
2019-09-10 16:19:06 -0500

    Updates

---
+++ precache-manifest.e3ad39ade4495e24250a662b22519a14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89fcc18dd70ff9cf5911bfa2448dc59e",
+    "url": "/clicktest/index.html"
+  },
+  {
+    "revision": "17e5a084c2653867484a",
+    "url": "/clicktest/static/css/main.b78a199f.chunk.css"
+  },
+  {
+    "revision": "6c02683395e0ca10a9e8",
+    "url": "/clicktest/static/js/2.641c4581.chunk.js"
+  },
+  {
+    "revision": "17e5a084c2653867484a",
+    "url": "/clicktest/static/js/main.385081ac.chunk.js"
+  },
+  {
+    "revision": "65cb0ffa0366b4733ad5",
+    "url": "/clicktest/static/js/runtime~main.254ef3ac.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ClickyGame/precache-manifest.f10ee24be2cc1d7885008d9036f85e4d.js"
+  "/clicktest/precache-manifest.e3ad39ade4495e24250a662b22519a14.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ClickyGame/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clicktest/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
