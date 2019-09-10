b576d850f5eaa2d2e9cd1d65bcf975d4de86d751
User <User@Apples-MacBook-Pro-1113.local>
2019-09-10 16:14:50 -0500

    Updates

---
+++ precache-manifest.f10ee24be2cc1d7885008d9036f85e4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5faef8cf6293215ddc230c6233a9370c",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "410f3e8642928553d65b",
+    "url": "/ClickyGame/static/css/main.b78a199f.chunk.css"
+  },
+  {
+    "revision": "6c02683395e0ca10a9e8",
+    "url": "/ClickyGame/static/js/2.641c4581.chunk.js"
+  },
+  {
+    "revision": "410f3e8642928553d65b",
+    "url": "/ClickyGame/static/js/main.d5165a97.chunk.js"
+  },
+  {
+    "revision": "6b9aa63014a7910a0c98",
+    "url": "/ClickyGame/static/js/runtime~main.ebff308b.js"
+  }
+]);
